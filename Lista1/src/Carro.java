a3e1234db8f58977a107683a0d29e627b7bea430
giovallu <giovallu.st@gmail.com>
2025-02-24 21:44:43 -0300

    aula de Java FACEF

---
+++ Lista1/src/Carro.java
@@ -0,0 +1,54 @@
+import javax.swing.*;
+
+public class Carro {
+    //classe é composta de variáveis e métodos
+
+    //variáveis
+    //do tipo primitivo
+
+    public int ano;
+
+    public float velocidade;
+
+    //do tipo classe
+
+    public String marca, modelo;
+
+    // métodos construtores
+
+    public Carro() {
+        this.marca = "Marca Inválida";
+        this.modelo = "Modelo Inválida";
+    }
+
+    public Carro(int ano, float velocidade, String marca, String modelo) {
+        this.ano = ano;
+        this.velocidade = velocidade;
+        this.marca = marca;
+        this.modelo = modelo;
+    }
+
+    //métodos
+     public void exibirDetalhes(){
+         /*this representa o objeto que chama o método */
+         JOptionPane.showMessageDialog(null,"Marca: " + this.marca +
+                 " \nModelo: " + this.modelo +
+                 " \nAno: " + this.ano +
+                 " \nVelocidade: " + this.velocidade
+                 + "\n________________________________");
+     }
+
+     // vai acelerar o carro de x unidades
+    public void acelerar(float x){
+        this.velocidade = this.velocidade + x;
+    }
+
+    public void frear(float x) {
+        if (this.velocidade-x <= 0) {
+            System.out.println("Impossível frear");
+        }else {
+            this.velocidade -= x;
+        }
+    }
+
+}
---
+++ Lista1/src/Main.java
@@ -0,0 +1,36 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        // criando objetos da classe
+        // instanciar a classe = criando objetos
+
+        Carro obj1 = new Carro(); // chama o construtor
+        obj1.ano = 2022;
+        obj1.velocidade = 0;
+        obj1.marca = "Volkswagen";
+        obj1.modelo = "Polo";
+        obj1.exibirDetalhes();
+        obj1.acelerar(100);
+        obj1.exibirDetalhes();
+        obj1.frear(10);
+        obj1.exibirDetalhes();
+
+        Carro obj2 = new Carro(); // chama o construtor
+        obj2.ano = 2024;
+        obj2.velocidade = 0;
+        obj2.marca = "Ford";
+        obj2.modelo = "FastBack";
+        obj2.exibirDetalhes();
+
+        Carro obj3 = new Carro(); // chama o construtor
+        obj3.exibirDetalhes();
+
+        /*O segundo método construtor serve para facilitar a criação de objetos*/
+        Carro obj4 = new Carro(2025, 0, "Chevrolet", "Cruze");
+        obj4.exibirDetalhes();
+        obj4.frear(10);
+        obj4.exibirDetalhes();
+
+    }
+}
